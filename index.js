7c3abb3c24046f87611c761736c62e5c656b27b6
andyhu <huyifan@vip.sina.com>
2015-02-03 23:13:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function send(ctx, path, opts) {
   var index = opts.index;
   var maxage = opts.maxage || opts.maxAge || 0;
   var hidden = opts.hidden || false;
-  var gzip = opts.gzip || true;
+  var gzip = !!opts.gzip;
 
   return function *(){
     var trailingSlash = '/' == path[path.length - 1];
