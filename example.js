cbf0610f2619a844f9f68b24eba0c39d6b76a2ae
haoxin <haoxins@outlook.com>
2017-04-26 19:10:44 +0800

    lint

--- example.js
+++ example.js
@@ -1,15 +1,18 @@
 
-const send = require('./');
-const Koa = require('koa');
-const app = new Koa();
+const send = require('./')
+const Koa = require('koa')
+const app = new Koa()
 
 // $ GET /package.json
 // $ GET /
 
 app.use(async (ctx) => {
-  if ('/' == ctx.path) return ctx.body = 'Try GET /package.json';
-  await send(ctx, ctx.path, { root: __dirname });
+  if (ctx.path === '/') {
+    ctx.body = 'Try GET /package.json'
+    return
+  }
+  await send(ctx, ctx.path, { root: __dirname })
 })
 
-app.listen(3000);
-console.log('listening on port 3000');
+app.listen(3000)
+console.log('listening on port 3000')
